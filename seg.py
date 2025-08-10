b8410ddb20ebb0f031b525f2147ebc9d4d43a635
cookerae <cookstronekuoler@gmail.com>
2025-08-10 17:22:14 +0800

    Update seg.py
    Signed-off-by: cookerae <cookstronekuoler@gmail.com>

--- seg.py
+++ seg.py
@@ -3,22 +3,24 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('D:/工程创新/最终训练/ultralytics-main/ultralytics-main/ultralytics/cfg/models/11/yolo11.yaml')
+    model = YOLO('/content/ultralytics/ultralytics/cfg/models/11/yolo11s-segoper.yaml')
     #model.load('yolov8n.pt') # loading pretrain weights
-    model.train(data='D:/工程创新/最终训练/ultralytics-main/ultralytics-main/breast_tumor04/data.yaml',
+    model.train(data='/content/datasets/breast_tumor04/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=20,
-                batch= 4,
-                close_mosaic=10,
+                epochs=500,
+                batch= 16,
+                close_mosaic=0,
                 device='0',
                 optimizer='AdamW', # using BGD
+                lr0= 0.001,  # 较小的初始学习率
+                momentum= 0.9,
                 project='runs/train',
                 name='exp',
-                patience=10,
+                patience=150,
                 workers=0,
                 val=True,
-                amp=True,
+                amp=False,
                 save_period = 5,
                 #python train.py --img 640 --batch 16 --epochs 400 --data your_dataset.yaml --weights yolov8n.pt --patience 100 --close_mosaic 0 --name Stage1_Mosaic_Training
                 #python train.py --img 640 --batch 16 --epochs 50 --data your_dataset.yaml --weights runs/train/Stage1_Mosaic_Training/weights/best.pt --patience 30 --lr0 0.001 --no-aug --name Stage2_Finetuning

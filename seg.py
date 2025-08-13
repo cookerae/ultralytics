8d3f496683b0137b80bc496632cd34c05f0d3544
cookerae <cookstronekuoler@gmail.com>
2025-08-13 14:19:44 +0800

    Update seg.py
    Signed-off-by: cookerae <cookstronekuoler@gmail.com>

--- seg.py
+++ seg.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
                 cache=False,
                 imgsz=640,
                 epochs=500,
-                batch= 48,
+                batch= 32,
                 close_mosaic=0,
-                device='0',
+                device='0,1',
                 optimizer='AdamW', # using BGD
                 lr0= 0.001,  # 较小的初始学习率
                 momentum= 0.9,

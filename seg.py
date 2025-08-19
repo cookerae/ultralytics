0053905bbe2bf46dc22875b6db8e046180a2a0cb
cookerae <cookstronekuoler@gmail.com>
2025-08-19 21:11:33 +0800

    Update seg.py
    Signed-off-by: cookerae <cookstronekuoler@gmail.com>

--- seg.py
+++ seg.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
                 #data='/content/datasets/breast_tumor04/data.yaml'
                 cache=False,
                 imgsz=640,
-                epochs=500,
+                epochs=600,
                 batch= 32,
-                close_mosaic=0,
+                close_mosaic=250,
                 device='0,1',
                 optimizer='AdamW', # using BGD
                 lr0= 0.0005, # 较小的初始学习率

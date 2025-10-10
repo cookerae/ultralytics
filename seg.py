5f4b31f2624de7dd6317e496d6df3f790716b6d4
cookerae <cookstronekuoler@gmail.com>
2025-10-10 16:11:32 +0800

    Update seg.py
    Signed-off-by: cookerae <cookstronekuoler@gmail.com>

--- seg.py
+++ seg.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                 cache=False,
                 imgsz=640,
                 epochs=300,
-                batch= 32,
+                batch= 64,
                 close_mosaic=0,
                 device='0,1',
                 optimizer='AdamW', # using BGD

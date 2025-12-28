d1e70ea24ef0d3a8dff65fc8c2fe27dc8efa696d
cookerae <cookstronekuoler@gmail.com>
2025-12-28 20:25:25 +0800

    Update train.py
    Signed-off-by: cookerae <cookstronekuoler@gmail.com>

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/kaggle/working/ultralytics/ultralytics/cfg/models/11/yolo11s-oper04.yaml')
+    model = YOLO('/kaggle/working/ultralytics/ultralytics/cfg/models/11/yolo11s-oper03.yaml')
     #model.load('/content/drive/MyDrive/KaggleNotebookOutput/ultralytics/runs/train/exp/weights/best.pt') # loading pretrain weights
     #model.load('/kaggle/input/tumor-yolo/finally-best.pt') # loading pretrain weights
     model.train(#data='/root/.cache/kagglehub/datasets/monaerkiconbinker/tumor-yolo/versions/2/breast_tumor04/data.yaml',
